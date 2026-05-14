5c3827c5e1bc0a6388544102e5952513cad74542
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-05-14 19:07:10 -0300

    Factory function module for creating tasks

--- src/tasks.js
+++ src/tasks.js
@@ -1,27 +1,14 @@
+// Get main content element
 function getMainElement() {
   return document.getElementById('content');
 };
 
-// Load placeholder tasks
-function loadCreatedTasks() {
-
-  // Project specific title
-  const projetTitle = document.createElement("h2");
-  projetTitle.textContent = "Newly Created";
-  getMainElement().appendChild(projetTitle);
-
-  // Tasks list items
-  const tasksList = document.createElement("ul");
-  const tasksListItems1 = document.createElement("li");
-  tasksListItems1.textContent = "Project Task #1";
-  const tasksListItems2 = document.createElement("li");
-  tasksListItems2.textContent = "Project Task #2";
-
-  // Add created items
-  tasksList.appendChild(tasksListItems1);
-  tasksList.appendChild(tasksListItems2);
-  getMainElement().appendChild(tasksList);
-
+// Tasks factory function module
+function createTask(title, description, dueDate, priority) {
+  return {title, description, dueDate, priority};
 };
 
-export { loadCreatedTasks };
+// Stora default project tasks
+const defaultProjectTasks = [];
+
+export { renderTasks };
