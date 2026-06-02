fdab75d6da5ef2fa5da45d5788f60823adee4931
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-06-02 20:02:44 -0300

    Add, get, store and factory for the Projects module

--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,29 @@
+const projects = [];
+
 // Get sidebar projects element
 function getSidebarProjectsEl() {
     return document.querySelector('.sidebar-projects');
 };
 
-// Projects factory function module
+// Projects factory module
 function createProject(title) {
-    return { title };
+    return { title, tasks: [] };
+};
+
+// Add new project
+function addProject(title) {
+    const project = createProject(title);
+    projects.push(project);
+    return project;
+};
+
+// Get all projects
+function getProjects() {
+    return projects;
+};
+
+export {
+    addProject,
+    getProjects,
+    createProject
 };
