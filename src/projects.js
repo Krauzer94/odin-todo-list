2697ecc988c6816a13479dc8d8f0bc57ff7eb5bc
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-06-02 19:43:45 -0300

    Initial projects handling module (add, edit, remove)

---
+++ src/projects.js
@@ -0,0 +1,9 @@
+// Get sidebar projects element
+function getSidebarProjectsEl() {
+    return document.querySelector('sidebar-projects');
+};
+
+// Projects factory function module
+function createProject(title) {
+    return { title };
+};
