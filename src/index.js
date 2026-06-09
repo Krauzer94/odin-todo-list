77f292d36a1324d026cfae9c8f1fce63236d02af
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-06-09 19:24:30 -0300

    Import newly created DOM rendering module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import './style.css';
 
+import { renderProjects } from './dom.js';
+import { addProject } from './projects.js';
 import { loadCreatedTasks } from './tasks.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Loads created tasks
-  function _loadCreatedTasks() {
-    loadCreatedTasks();
-  };
+  // Add default project
+  addProject('Default Project');
+
+  // Render sidebar projects
+  renderProjects();
 
   // Render mock tasks
   const mainElement = document.getElementById('content');
   mainElement.id = 'content';
-  _loadCreatedTasks();
+  loadCreatedTasks();
 
 });
