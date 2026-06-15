4ba004827fba387b38ce9e6b3f694e64ee298b2a
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-06-15 14:29:59 -0300

    Identation

--- src/projects.js
+++ src/projects.js
@@ -2,28 +2,31 @@ const projects = [];
 
 // Get sidebar projects element
 function getSidebarProjectsEl() {
-    return document.querySelector('.sidebar-projects');
-};
+  return document.querySelector(".sidebar-projects");
+}
 
 // Projects factory module
 function createProject(title) {
-    return { title, tasks: [] };
-};
+  return {
+    title,
+    tasks: []
+  };
+}
 
 // Add new project
 function addProject(title) {
-    const project = createProject(title);
-    projects.push(project);
-    return project;
-};
+  const project = createProject(title);
+  projects.push(project);
+  return project;
+}
 
 // Get all projects
 function getProjects() {
-    return projects;
-};
+  return projects;
+}
 
 export {
-    addProject,
-    getProjects,
-    createProject
+  addProject,
+  getProjects,
+  createProject
 };
