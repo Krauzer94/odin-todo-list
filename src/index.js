8ef7d4a69eabba7e71e84f2db2712e4898760074
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-07-13 19:15:20 -0300

    Logic to render and trigger side-bar interaction

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import "./style.css";
 
-import { loadCreatedTasks } from "./tasks.js";
+import { loadCreatedTasks, renderTasks } from "./tasks.js";
+import { initProjectModal, renderSidebarProjects } from "./projects-dom.js";
+import { addProject, getProjects } from "./projects.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Render mock tasks
-  const mainElement = document.getElementById("content");
-  mainElement.id = "content";
+  // Initialize project modal logic
+  initProjectModal({
+    onSubmit: (title) => {
+      addProject(title);
+      renderSidebarProjects();
+    },
+  });
+
+  // Hook up sidebar add button
+  const addProjectBtn = document.querySelector(".add-project");
+  const addProjectDialog = document.getElementById("addProjectDialog");
+  addProjectBtn.addEventListener("click", () => {
+    addProjectDialog.showModal();
+  });
+
+  // Load generated content
   loadCreatedTasks();
 });
